72dfe56c9907bb452f22001eb2d6062d18fc0fd2
kaustubh.dhokte <kaustubh.dhokte@gmail.com>
2020-08-21 19:53:21 -0400

    works fine

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,46 @@
-kubectl create -f config/manager/namespace.yaml
+echo "Creating namespace ... "
 
-eksctl create iamserviceaccount --name cloudfservice --namespace cloudformationcrd-system --cluster $1 --attach-policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess --approve
+# kubectl create -f config/manager/namespace.yaml
+
+echo '###################################################################################################'
+
+echo "Creating iamserviceaccount ... "
+
+# eksctl create iamserviceaccount \
+# --name awsobjects \
+# --namespace cloudformationcrd-system \
+# --cluster $1 \
+# --attach-policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess \
+# --attach-policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess \
+# --attach-policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess \
+# --approve
+
+echo '###################################################################################################'
+
+echo "Creating resources ... "
 
 make install
 
+echo '###################################################################################################'
+
+echo "Manager image Build and push ... "
+
 make docker-build docker-push IMG=$2
+
+echo '###################################################################################################'
+
+echo "Deploying the controller image ... "
 
 make deploy IMG=$2
 
-kubectl create -f config/samples/cloudf_v1_ec2instance.yaml
+echo '###################################################################################################'
+
+echo "Creating sample resources in AWS ... "
+
+#kubectl create -f config/samples/cloudf_v1_ec2instance.yaml
+
+echo '###################################################################################################'
+
+echo "Done !!!"
+
+echo "Verify created resources through AWS management console."
